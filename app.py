e2eda70880380e9a884b833be4e0ba88dd7af021
Dmitry <dadonov.mitia2016@yandex.ru>
2024-10-25 18:29:48 +0300

    work version

--- app.py
+++ app.py
@@ -1,55 +1,24 @@
 import sys
-import cmath
 import math
-
-PI = math.pi
-E = math.e
-
-
-from typing import Union, Optional
-from operator import add, sub, mul, truediv, abs, pow
+import re
 
 from PySide6.QtWidgets import QApplication, QMainWindow
-
 from Calculator import Ui_MainWindow
-
-operations = {
-        '+': add,
-        '-': sub,
-        '×': mul,
-        '/': truediv,
-        'mod': abs,
-        '^': pow,
-        '\u221a': lambda x: math.sqrt(x)
-}
-
-error_zero_div = 'Division by zero'
-error_undefined = 'Result in undefined'
 
 class Calculator(QMainWindow):
     def __init__(self):
         super(Calculator, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
         self.entry = self.ui.lineEdit
         self.temp = self.ui.lineEdit_2
+        self.entry.setText("")  # Начальное пустое поле
 
-        self.entry_max_len = self.entry.maxLength()
+        # Числовые кнопки
+        for i in range(10):
+            getattr(self.ui, f'pushButton_{i}').clicked.connect(self.add_digit)
 
-        # числа
-        self.ui.pushButton_0.clicked.connect(self.add_digit)
-        self.ui.pushButton_1.clicked.connect(self.add_digit)
-        self.ui.pushButton_2.clicked.connect(self.add_digit)
-        self.ui.pushButton_3.clicked.connect(self.add_digit)
-        self.ui.pushButton_4.clicked.connect(self.add_digit)
-        self.ui.pushButton_5.clicked.connect(self.add_digit)
-        self.ui.pushButton_6.clicked.connect(self.add_digit)
-        self.ui.pushButton_7.clicked.connect(self.add_digit)
-        self.ui.pushButton_8.clicked.connect(self.add_digit)
-        self.ui.pushButton_9.clicked.connect(self.add_digit)
-
-    #действия
+        # Кнопки операций
         self.ui.pushButton_clear.clicked.connect(self.clear_all)
         self.ui.pushButton_point.clicked.connect(self.add_point)
         self.ui.pushButton_negate.clicked.connect(self.negate)
@@ -57,41 +26,55 @@ class Calculator(QMainWindow):
         self.ui.pushButton_bracket_opening.clicked.connect(self.add_bracket_opening)
         self.ui.pushButton_bracket_closing.clicked.connect(self.add_bracket_closing)
 
-    #математика
+        # Математические операции
         self.ui.pushButton_equals.clicked.connect(self.calculate)
-        self.ui.pushButton_plus.clicked.connect(self.math_operation)
-        self.ui.pushButton_minus.clicked.connect(self.math_operation)
-        self.ui.pushButton_mul.clicked.connect(self.math_operation)
-        self.ui.pushButton_divivde.clicked.connect(self.math_operation)
-        self.ui.pushButton_power.clicked.connect(self.math_operation)
-        self.ui.pushButton_mod.clicked.connect(self.math_operation)
-        self.ui.pushButton_percent.clicked.connect(self.add_percent)
-        self.ui.pushButton_sqrt.clicked.connect(self.math_operation)
+        self.ui.pushButton_plus.clicked.connect(lambda: self.add_operation('+'))
+        self.ui.pushButton_minus.clicked.connect(lambda: self.add_operation('-'))
+        self.ui.pushButton_mul.clicked.connect(lambda: self.add_operation('*'))
+        self.ui.pushButton_divide.clicked.connect(lambda: self.add_operation('/'))
+        self.ui.pushButton_power.clicked.connect(lambda: self.add_operation('**'))
+        self.ui.pushButton_percent.clicked.connect(lambda: self.add_operation('%'))
 
-    #константы
-        self.ui.pushButton_E.clicked.connect(self.add_digit)
-        self.ui.pushButton_PI.clicked.connect(self.add_digit)
 
-    def add_digit(self,btn_text: str) -> None:
-        self.remove_error()
+
+        # Константы и тригонометрические функции
+        self.ui.pushButton_E.clicked.connect(lambda: self.add_constant(math.e))
+        self.ui.pushButton_PI.clicked.connect(lambda: self.add_constant(math.pi))
+        self.ui.pushButton_sqrt.clicked.connect(lambda: self.add_function("sqrt"))
+        self.ui.pushButton_sin.clicked.connect(lambda: self.add_function("sin"))
+        self.ui.pushButton_cos.clicked.connect(lambda: self.add_function("cos"))
+        self.ui.pushButton_tan.clicked.connect(lambda: self.add_function("tan"))
+        self.ui.pushButton_cot.clicked.connect(lambda: self.add_function("cot"))
+        self.ui.pushButton_arcsin.clicked.connect(lambda: self.add_function("arcsin"))
+        self.ui.pushButton_arccos.clicked.connect(lambda: self.add_function("arccos"))
+        self.ui.pushButton_arctan.clicked.connect(lambda: self.add_function("arctan"))
+        self.ui.pushButton_arccot.clicked.connect(lambda: self.add_function("arccot"))
+        self.ui.pushButton_ln.clicked.connect(lambda: self.add_function("ln"))
+        self.ui.pushButton_log.clicked.connect(lambda: self.add_function("log"))
+        self.ui.pushButton_mod.clicked.connect(lambda: self.add_function("mod"))
+
+    def add_digit(self):
         self.clear_temp_if_equality()
         btn = self.sender()
+        self.entry.setText(self.entry.text() + btn.text())
 
-        digit_buttons = ('pushButton_0', 'pushButton_1', 'pushButton_2', 'pushButton_3', 'pushButton_4', 'pushButton_5',
-                         'pushButton_6', 'pushButton_7', 'pushButton_8', 'pushButton_9', 'pushButton_E', 'pushButton_PI',
-                         'pushButton_bracket1', 'pushButton_bracket2', 'pushButton_sqrt', 'pushButton_mod', 'pushButton_percent',
-                         'pushButton_sqrt', 'pushButton_power')
+    def add_function(self, function: str):
+        self.clear_temp_if_equality()
+        self.entry.setText(self.entry.text() + f"{function}(")
 
-        if btn.objectName() in digit_buttons:
-            if self.entry.text() == '0':
-                self.entry.setText(btn.text())
-            else:
-                self.entry.setText(self.entry.text() + btn.text())
+    def add_operation(self, operation: str):
+        self.clear_temp_if_equality()
+        current_text = self.entry.text()
+        if current_text and current_text[-1] not in "+-*/**":
+            self.entry.setText(current_text + operation)
 
-    def add_point(self) -> None:
+    def add_point(self):
         self.clear_temp_if_equality()
         if '.' not in self.entry.text():
-            self.entry.setText(self.entry.text() + '.')
+            if not self.entry.text():
+                self.entry.setText("0.")
+            else:
+                self.entry.setText(self.entry.text() + '.')
 
     def add_bracket_opening(self) -> None:
         self.entry.setText(self.entry.text() + '(')
@@ -99,155 +82,70 @@ class Calculator(QMainWindow):
     def add_bracket_closing(self) -> None:
         self.entry.setText(self.entry.text() + ')')
 
-    def add_percent(self) -> None:
-        self.entry.setText(self.entry.text() + '%')
-
     def negate(self):
+        entry = self.entry.text()
+        if entry and entry[0] != '-':
+            self.entry.setText('-' + entry)
+        elif entry:
+            self.entry.setText(entry[1:])
+
+    def backspace(self):
         self.clear_temp_if_equality()
         entry = self.entry.text()
+        if entry:
+            self.entry.setText(entry[:-1])
 
-        if '-' not in entry:
-            if entry != '0':
-                entry = '-' + entry
-        else:
-            entry = entry[1:]
-
-        if len(entry) == self.entry_max_len + 1 and '-' in entry:
-            self.entry.setMaxLength(self.entry_max_len + 1)
-        else:
-            self.entry.setMaxLength(self.entry_max_len)
-
-        self.entry.setText(entry)
-
-    def backspace(self) -> None:
-        self.remove_error()
-        self.clear_temp_if_equality()
-        entry = self.entry.text()
-
-        if len(entry) != 1:
-            if len(entry) == 2 and '-' in entry:
-                self.entry.setTtext('0')
-            else:
-                self.entry.setText(entry[:-1])
-        else:
-            self.entry.setText('0')
-
-    def clear_all(self) -> None:
-        self.remove_error()
-        self.entry.setText('0')
+    def clear_all(self):
+        self.entry.setText("")
         self.temp.clear()
 
-    def clear_entry(self) -> None:
-        self.remove_error()
+    def add_constant(self, constant: float):
         self.clear_temp_if_equality()
-        self.entry.setText('0')
+        self.entry.setText(self.entry.text() + str(constant))
 
-
-    def add_temp(self):
-        btn = self.sender()
-        entry = self.remove_trailing_zeros(self.entry.text())
-
-        if not self.temp.text() or self.get_math_sign() == '=':
-            self.temp.setText(entry + f' {btn.text()} ')
-            self.entry.setText('0')
-
-    def get_entry_num(self) -> Union[int, float]:
-        entry = self.entry.text().strip('.')
-
-        return float(entry) if '.' in entry else int(entry)
-
-    def get_temp_num(self) -> Union[int, float, None]:
-        if self.temp.text():
-            temp = self.temp.text().strip('.').split()[0]
-            return float(temp) if '.' in temp else int(temp)
-
-    def get_math_sign(self) -> Optional[str]:
-        if self.temp.text():
-            return self.temp.text().strip('.').split()[-1]
-
-    def calculate(self) -> Optional[str]:
-        entry = self.entry.text()
-        temp = self.temp.text()
-
-        if temp:
-            try:
-                result = self.remove_trailing_zeros(
-                    str(operations[self.get_math_sign()](self.get_temp_num(), self.get_entry_num()))
-                    )
-                self.temp.setText(temp + self.remove_trailing_zeros(entry) +  ' =')
-                self.entry.setText(result)
-                return result
-
-            except KeyError:
-                pass
-
-            except ZeroDivisionError:
-                if self.get_temp_num() == 0:
-                    self.show_error(error_undefined)
-                else:
-                    self.show_error(error_zero_div)
-
-    def math_operation(self) -> None:
-        temp = self.temp.text()
-        btn = self.sender()
-
-        try:
-            if not temp:
-                self.add_temp()
-            else:
-                if self.get_math_sign() != btn.text():
-                    if self.get_math_sign() == '=':
-                        self.add_temp()
-                    else:
-                        self.temp.setText(temp[:-2] + f'{btn.text()}')
-                else:
-                    self.temp.setText(self.calculate() + f'{btn.text()}')
-        except TypeError:
-            pass
-
-    def show_error(self, text: str) -> None:
-        self.entry.setMaxLength(len(text))
-        self.entry.setText(text)
-        self.disable_buttons(True)
-
-    def remove_error(self) -> None:
-        if self.entry.text() in (error_undefined,error_zero_div):
-            self.entry.setMaxLength(self.entry_max_len)
-            self.entry.setText('0')
-            self.disable_buttons(False)
-
-    def disable_buttons(self,disable: bool) -> None:
-        self.ui.pushButton_equals.setDisabled(disable)
-        self.ui.pushButton_plus.setDisabled(disable)
-        self.ui.pushButton_minus.setDisabled(disable)
-        self.ui.pushButton_mul.setDisabled(disable)
-        self.ui.pushButton_divivde.setDisabled(disable)
-        self.ui.pushButton_negate.setDisabled(disable)
-        self.ui.pushButton_point.setDisabled(disable)
-
-    #     self.change_buttons_color('color: #888;')
-    #
-    # def change_buttons_color(self, css_color: str) -> None:
-    #     self.ui.pushButton_equals.setDisabled(css_color)
-    #     self.ui.pushButton_plus.setDisabled(css_color)
-    #     self.ui.pushButton_minus.setDisabled(css_color)
-    #     self.ui.pushButton_mul.setDisabled(css_color)
-    #     self.ui.pushButton_divivde.setDisabled(css_color)
-    #     self.ui.pushButton_negate.setDisabled(css_color)
-    #     self.ui.pushButton_point.setDisabled(css_color)
-
-    def clear_temp_if_equality(self) -> None:
-        if self.get_math_sign() == '=':
+    def clear_temp_if_equality(self):
+        if '=' in self.temp.text():
             self.temp.clear()
 
-    @staticmethod
-    def remove_trailing_zeros(num: str) -> str:
-        n = str(float(num))
-        return n[:-2] if n[-2:] == '.0' else n
+    def calculate(self):
+        expression = self.entry.text()
+        self.temp.setText(expression)  # Отображаем выражение в temp
+
+        # Заменяем проценты, например, "50%" на "50*0.01"
+        expression = re.sub(r'(\d+(\.\d+)?)%', r'(\1*0.01)', expression)
+
+        # Контекст для функций
+        context = {
+            "sqrt": math.sqrt,
+            "sin": lambda x: math.sin(math.radians(x)),
+            "cos": lambda x: math.cos(math.radians(x)),
+            "tan": lambda x: math.tan(math.radians(x)),
+            "cot": lambda x: 1 / math.tan(math.radians(x)),
+            "arcsin": lambda x: math.degrees(math.asin(x)),
+            "arccos": lambda x: math.degrees(math.acos(x)),
+            "arctan": lambda x: math.degrees(math.atan(x)),
+            "arccot": lambda x: 1/ math.degrees(math.atan(x)),
+            "ln": math.log,
+            "log": lambda x, base=10: math.log(x, base),
+            "mod": lambda x : abs(x),
+            "pi": math.pi,
+            "e": math.e
+        }
+
+        try:
+            # Вычисляем выражение с помощью eval
+            result = eval(expression, {"__builtins__": None}, context)
+            self.entry.setText(str(result))
+        except ZeroDivisionError:
+            self.show_error("Division by zero")
+        except Exception:
+            self.show_error("Error")
+
+    def show_error(self, text):
+        self.entry.setText(text)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     window = Calculator()
     window.show()
     sys.exit(app.exec())
